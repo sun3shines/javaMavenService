8dbf4c331051e3118febe648b2653f200a38058c
sheshisheng <sheshisheng@okay.cn>
2019-04-15 19:13:07 +0800

    update

---
+++ src/main/java/okay/mybatisservice/mapper/UserMapper.java
@@ -0,0 +1,6 @@
+package okay.mybatisservice.mapper;
+
+public interface UserMapper {
+
+    public User selectUser(int id);
+}
--- src/main/java/okay/mybatisservice/mybatisService.java
+++ src/main/java/okay/mybatisservice/mybatisService.java
@@ -1,6 +1,7 @@
 package okay.mybatisservice;
 
 import okay.mybatisservice.mapper.User;
+import okay.mybatisservice.mapper.UserMapper;
 import okay.mysqlservice.MysqlService;
 import org.apache.ibatis.session.SqlSession;
 
@@ -11,8 +12,11 @@ public class mybatisService {
 
         SqlSession sqlSession = MysqlService.getSession();
 
-        User  user  = sqlSession.selectOne("selectUser", 1);
-        System.out.println(user);
+//        User  user  = sqlSession.selectOne("selectUser", 1);
+//        System.out.println(user);
 
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User u = mapper.selectUser(1);
+        System.out.println(u);
     }
 }
