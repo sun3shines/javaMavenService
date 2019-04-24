3db031f97ed761daaf2f04f0e9dafa7ca0404e91
sheshisheng <sheshisheng@okay.cn>
2019-04-24 19:33:11 +0800

    update

--- src/main/java/okay/dubboprovider/HelloServiceImpl.java
+++ src/main/java/okay/dubboprovider/HelloServiceImpl.java
@@ -3,6 +3,7 @@ package okay.dubboprovider;
 public class HelloServiceImpl  implements  HelloService{
     @Override
     public String say(String name) {
+        System.out.println("===");
         return "My name is " + name;
     }
 }
--- src/main/java/okay/dubboprovider/ProviderService.java
+++ src/main/java/okay/dubboprovider/ProviderService.java
@@ -1,7 +1,7 @@
 package okay.dubboprovider;
 
 public class ProviderService {
-    
+
     public static void main(String[] args) throws Exception {
 
         new BaseService<DemoService>().getService(DemoService.class, DemoServiceImpl.class).export();
